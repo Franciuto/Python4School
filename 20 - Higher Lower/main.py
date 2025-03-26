3f5f36053bd5991156572d199b51288ae7c92fad
Franciuto <francesco.traversetolo@gmail.com>
2025-03-26 22:00:22 +0100

    Aggiunta di funzioni per la selezione casuale degli account e preparazione della tabella di gioco

--- 20 - Higher Lower/main.py
+++ 20 - Higher Lower/main.py
@@ -1,4 +1,20 @@
 from game_data import *
+import random
+import tabulate
 
-print('Benvenuto nel gioco high or lower\n\nℹ️ Istruzioni ℹ️\n ► Ti verranno presentate due opzioni\n ► Dovrai scegliere quella che reputi più seguita')
+print('Benvenuto nel gioco high or lower\n\nℹ Istruzioni ℹ\n ► Ti verranno presentate due opzioni\n ► Dovrai scegliere quella che reputi più seguita')
 
+def random_account():
+    already_solved = []
+    r = random.randint(0, len(data))       
+    while r in already_solved:
+        r = random.randint(0, len(data))
+    already_solved.append(r)
+    return data[r]
+
+def make_table(account1, account2):
+    
+    pass
+p1 = random_account()
+p2 = random_account()
+print(f'{p1}\n{p2}')
